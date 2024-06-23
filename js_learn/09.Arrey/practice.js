e1b2424293f2a37a5f62a752b4e9168c0d1af38c
rana <ranaparvesh0779@gmail.com>
2024-06-23 22:23:58 +0600

    23/06/24

--- js_learn/09.Arrey/practice.js
+++ js_learn/09.Arrey/practice.js
@@ -1,11 +1,11 @@
-/*function sleep(name, time) {
+function sleep(name, time) {
   console.log(name + "is sleeping at" + time);
 }
 
 sleep("rahim ", "9am");
 sleep("karim ", "10am");
 sleep("shoron ", "11am");
-*/
+
 //task
 
 var firstNumber = 20;
--- js_learn/10.function/index.js
+++ js_learn/10.function/index.js
@@ -3,7 +3,7 @@ function myFunction() {
   console.log(carname);
 }
 myFunction();
-
+/*
 var num1 = prompt(" enter first number :");
 var num2 = prompt(" enter secend number :");
 
@@ -17,3 +17,36 @@ document.write("addition =  " + sum);
 
 sub = num1 - num2;
 document.write("substraction =  " + sub);
+*/
+//
+
+function sleep(name, time) {
+  console.log(name + "is sleeping at" + time);
+}
+
+sleep("rahim ", "9am");
+sleep("karim ", "10am");
+sleep("shoron ", "11am");
+
+//task
+
+var firstNumber = 20;
+var secondNumber = 10;
+
+var sum, sub, mul;
+var div, rem;
+
+sum = firstNumber + secondNumber;
+console.log("20  +  10 =", sum);
+
+sub = firstNumber - secondNumber;
+console.log("20  -  10 =", sub);
+
+mul = firstNumber * secondNumber;
+console.log("20  *  10 =", mul);
+
+div = firstNumber / secondNumber;
+console.log("20  /  10 =", div);
+
+rem = firstNumber % secondNumber;
+console.log("20  %  10 =", rem);
