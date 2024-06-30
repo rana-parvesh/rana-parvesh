02e84e6ddf908397e845ba844a02a3890ee8c5c3
rana <ranaparvesh0779@gmail.com>
2024-06-30 10:43:18 +0600

    anisul islam learn

--- js_learn/10.function/learn.js
+++ js_learn/10.function/learn.js
@@ -206,7 +206,7 @@ var sum = 0;
 for (var i = m; i <= n; i++) {
   sum += i;
 }
-document.write(sum);*/
+document.write(sum);
 
 //
 
@@ -219,3 +219,223 @@ for (var i = 0; i <= 0; i++) {
   document.write("This is the value (total taka)  =  ");
 }
 document.write(totalTaka);
+
+var result = 100;
+if (80 <= result) {
+  document.write("A+ = 4 ");
+} else if (75 <= result) {
+  document.write("A =3.75 ");
+} else if (70 <= result) {
+  document.write("A- =3.50 ");
+} else if (65 <= result) {
+  document.write("B+ = 3.25 ");
+} else if (60 <= result) {
+  document.write("B = 3.00 ");
+} else if (55 <= result) {
+  document.write("B- = 2.75 ");
+} else if (50 <= result) {
+  document.write("C+ =2.50 ");
+} else if (45 <= result) {
+  document.write("C = 2.25");
+} else if (40 <= result) {
+  document.write("D = 2.00");
+} else if (0 <= result) {
+  document.write("F");
+}
+<br></br>;
+//
+{
+  const s1 = parseInt(prompt("Enter your Accounting :  "));
+  const s2 = parseInt(prompt("Enter your Management :  "));
+  const s3 = parseInt(prompt("Enter your Marketing :  "));
+  const s4 = parseInt(prompt("Enter your Law :  "));
+  const s5 = parseInt(prompt("Enter your Taxation :  "));
+  const s6 = parseInt(prompt("Enter your Statistics :  "));
+  const s7 = parseInt(prompt("Enter your Behavior :  "));
+  const s8 = parseInt(prompt("Enter your Insurance :  "));
+  const s9 = parseInt(prompt("Enter your Operation Management :  "));
+  const totalPoints = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9;
+  document.write("\n\nTotal Points = ", totalPoints, "\n");
+
+  const GPA = totalPoints / 9;
+  document.write("GPA = ", GPA);
+}
+
+
+//while
+
+var i = 1;
+sum = 0;
+
+while (i <= 50) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    sum = sum + i;
+  }
+  i++;
+}
+document.write("  ", sum);
+
+//do while loop
+
+var i = 11;
+do {
+  console.log("  " + i);
+  i++;
+} while (i <= 20);
+
+//break and contion
+
+for (i = 1; i <= 100; i++) {
+  if (i == 25) {
+    break;
+  }
+  if (i == 1) {
+  }
+  document.write(", ", i);
+}
+
+// condotion check with turnury operatior
+
+// var number = number(prompt("Enter your number : "));
+var number = parseInt(prompt(" Enter your number : "));
+var result = number > 0 ? "positive" : number < 0 ? "negetive" : "zero";
+document.write(result);
+
+
+
+// lets do the function
+
+var s = 4;
+var r = s * s;
+document.write("result = ", r, "<br>");
+
+var s = 5;
+var r = s * s;
+document.write("result = ", r, "<br>");
+
+var s = 6;
+var r = s * s;
+document.write("result = ", r, "<br>");
+
+//create in function
+
+function square() {
+  var s = 4;
+  var r = s * s;
+  document.write("result = ", r, "<br>");
+}
+//here is calling function
+
+square();
+square();
+square();
+
+
+//pass the paramiter
+
+function square(rana, shoron) {
+  var r = rana * shoron;
+  document.write("result = ", r, "<br>");
+}
+//here is calling function
+
+square(5, 8);
+square(6, 7);
+square(7, 3);
+
+//use return keyword
+
+function square(rana, shoron) {
+  var r = rana * shoron;
+  return r;
+}
+
+document.write("result = ", r, "<br>");
+document.write(square(5, 8));
+
+// addition function
+
+function add(tara, nana) {
+  result = tara + nana;
+  document.write("sum = ", result, "<br>");
+}
+add(25, 26);
+
+// this is sub function
+
+function sub(ta, na) {
+  result = ta - na;
+  document.write("sub = ", result);
+}
+sub(25, 26);
+
+// summary of this class:
+// 1. create a function
+// 2. calling a function
+// 3. paraiter pass
+// 4. returninhg value
+
+
+function rana(x, y) {
+  var result = x + y;
+  document.write(result, "<br>");
+}
+rana(25, 28);
+
+function sub(x, y) {
+  var result = x - y;
+  document.write(result, "<br>");
+}
+sub(25, 2);
+
+function mul(x, y) {
+  var result = x * y;
+  document.write(result, "<br>");
+}
+mul(5, 28);
+
+function vag(x, y) {
+  var result = x / y;
+  document.write(result);
+}
+// vag(25, 28);
+vag(50, 25);
+
+// task - 7 IIFES that print sum of two numbers (immidiyely)
+
+(function sum(x, y) {
+  var result = x + y;
+  document.write(result);
+})(2, 5);
+
+// function Expression
+var d2 = function displayMessage(msg) {
+  document.write(msg);
+};
+
+d2(" this is the function expression massage ");
+*/
+
+// this is the arrey
+
+var arrey = ["rana ", "shoron ", "zimu ", "raihan ", " hasi"];
+
+// for new variable
+arrey.push("muna");
+arrey.push("phota");
+arrey.push("ayon");
+document.write(arrey, "<br>");
+document.write(arrey.length, "<br>");
+document.write(arrey[5], "<br>");
+
+// for remove a varaiable
+
+arrey.pop();
+document.write(arrey, "<br>");
+
+// arrey concuting
+
+var n1 = [" hellow bangladesh  ", " how are you"];
+var n2 = [" hellow pakistan  ", " how are you"];
+var n = n1.concat(n2);
+document.write(n);
