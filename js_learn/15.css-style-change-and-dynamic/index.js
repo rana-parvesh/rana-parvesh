2d9de41039cf51d7c36d19d3c821090bc90f15d9
rana <ranaparvesh0779@gmail.com>
2024-07-08 09:05:41 +0600

    learning js

---
+++ js_learn/15.css-style-change-and-dynamic/index.js
@@ -0,0 +1,13 @@
+function add() {
+  var myVar = document.querySelector("#para");
+  myVar.classList.add("#para1");
+
+  //   myVar.style.color = "red";
+  //   myVar.style.fontSize = "3rem";
+  //   myVar.style.fontWeight = "bold";
+  //   myVar.style.fontStyle = "italic";
+}
+function remove() {
+  var myVar = document.querySelector("#para");
+  myVar.classList.remove("para1");
+}
