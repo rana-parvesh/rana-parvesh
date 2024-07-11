dbeda997cfa4bfc1728e922800812e37ca0ef86c
rana <ranaparvesh0779@gmail.com>
2024-07-11 22:55:49 +0600

    learning js

---
+++ Learning-Javascript/16.event-listener/index.js
@@ -0,0 +1,34 @@
+//single elements
+/*
+document.querySelector("button").addEventListener("click", rana);
+
+function rana() {
+  alert("hellow everyone");
+}
+
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseover", function () {
+  myVar.classList.add("my-style");
+});
+
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseout", function () {
+  myVar.classList.remove("my-style");
+});
+*/
+
+//multriple elements
+
+var len = document.querySelectorAll(".myButton").length; //for total lenghth
+
+//for re use this loop
+for (var i = 0; i < len; i++) {
+  document
+    .querySelectorAll(".myButton")
+    [i].addEventListener("click", function () {
+      var text = this.innerHTML;
+      document.querySelector("h1").innerHTML = text + "is clicked";
+    });
+}
---
+++ Learning-Javascript/17.audio-play-in-js/index.js
@@ -0,0 +1,30 @@
+var len = document.querySelectorAll(".my-button").length;
+
+for (var i = 0; i < len; i++) {
+  document
+    .querySelectorAll(".my-button")
+    [i].addEventListener("click", function () {
+      var text = this.innerHTML;
+      console.log(text);
+      play(text);
+    });
+}
+
+function play(text) {
+  switch (text) {
+    case "Button-A":
+      var audioA = new Audio("A.mp3");
+      audioA.play();
+      break;
+
+    case "Button-B":
+      var audioB = new Audio("`B.mp3`");
+      audioB.play();
+      break;
+
+    case "Button-C":
+      var audioC = new Audio("`C.mp3`");
+      audioC.play();
+      break;
+  }
+}
