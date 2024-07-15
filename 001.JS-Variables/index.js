41dfaf5e53c8aeed5b4d5ee2abdde3187950621c
rana <ranaparvesh0779@gmail.com>
2024-07-15 21:20:02 +0600

    new

---
+++ 001.JS-Variables/index.js
@@ -0,0 +1,63 @@
+// variable declare in 4 wayes now write some code
+
+//EXAMPLE
+
+x = 25;
+y = 23;
+z = x + y;
+console.log(z);
+
+// example use in var
+
+var a = 56;
+var b = 53;
+var c = a + b;
+console.log(c);
+
+// example to use let
+
+let s = 22;
+let r = 53;
+let d = s + r;
+console.log(d);
+
+//now use to the const
+const p = 22;
+const q = 3;
+const rana = p + q;
+console.log(rana);
+
+//and also use to the mixed variable
+
+const person1 = "rana";
+const person2 = " shoron";
+const relation = person1 + person2;
+console.log(relation, "they are brother");
+/*
+
+
+14/07/2024
+
+
+note :
+1. var is the global scope and it use anywhere.
+2. let is also the block scope and can not re use in any variables
+can not re declared from ang scope another time
+3. const must be assigned the value
+its use also new Array, Object, function, RegExp
+and also its a block scope the declaration
+
+//Example
+var x = 2;     // Allowed
+const x = 2;   // Not allowed
+
+{
+let x = 2;     // Allowed
+const x = 2;   // Not allowed
+}
+
+{
+const x = 2;   // Allowed
+const x = 2;   // Not allowed
+}
+*/
