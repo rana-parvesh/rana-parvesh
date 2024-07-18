679a542f40804a4a5e6b842947d9fc61c75c6a75
rana <ranaparvesh0779@gmail.com>
2024-07-18 12:25:35 +0600

    18/07/24

---
+++ 002.operator/index.js
@@ -0,0 +1,86 @@
+// there are to many operator
+// 1 . addtional operator ( + )
+
+var a = 10;
+var c = 10;
+var c = a + c;
+console.log(c);
+
+// 2 . substruction operator( - )
+
+var a = 10;
+var c = 10;
+var c = a - c;
+console.log(c);
+
+// 3 . multiplication operator( * )
+
+var a = 10;
+var c = 10;
+var c = a * c;
+console.log(c);
+
+// 4 . divition operator ( / )
+
+var a = 10;
+var c = 10;
+var c = a / c;
+console.log(c);
+
+// 5 . exponentiation operator( ** )
+var a = 5;
+var b = 3;
+var c = a ** b;
+console.log(c);
+// 6 . modulas operator ( % )
+
+var a = 15;
+var c = 10;
+var c = a % c;
+console.log(c);
+
+// 7  . increment operator ( ++ )
+
+var x = 10;
+var y = x++;
+console.log(x, y);
+
+// 7  . decrement operator ( -- )
+
+var x = 10;
+var y = x--;
+console.log(x, y);
+
+//this is the java script arithmetic operator
+/*
+Operator	Example	  Same As
+=	        x = y	  x = y  
++=	        x += y	  x = x + y
+-=	        x -= y	  x = x - y
+*=	        x *= y    x = x * y
+/=	        x /= y    x = x / y
+%=	        x %= y	  x = x % y
+**=      	x **= y	  x = x ** y
+*/
+
+// **JavaScript Comparison Operators
+
+/*
+Operator	  Description
+==	          equal to  // 10 =10
+===	          equal value and equal type // 10=10
+!=	          not equal //9!=10
+!==	          not equal value or not equal type a!==10
+>	          greater than // 10>9
+<	          less than// 10<3
+>=	          greater than or equal to //10>=10
+<=	          less than or equal to 3<=3
+?	          ternary operator // ?
+*/
+/*
+JavaScript Logical Operators
+Operator	Description
+&&	        logical and
+||	        logical or
+!	        logical not
+*/
