7e2a7c2881b6bd9a810425ee2b6ef4c848eb2b07
rana <ranaparvesh0779@gmail.com>
2024-09-11 21:52:18 +0600

    rana

--- Learning-Javascript/19.BOM/02.popup-boxes/index.js
+++ Learning-Javascript/19.BOM/02.popup-boxes/index.js
@@ -1,12 +1,12 @@
 //js BOM
-//popup box - alert, confirm, prompt
+//popup box - alert, confirm, prompt ....
 
 // alert("error here");
 
-/* 
-confirm
-confirm("are you sure?"); //output : yes = true, cancel = false
+/*
 
+// confirm
+confirm("are you sure?"); //output : yes = true, cancel = false 
 function rana() {
   let value = confirm("Do you want to delete?");
   if (value) {
@@ -19,15 +19,14 @@ rana();
 */
 
 //prompt
-function message() {
+function message(e = 10) {
   var h1 = document.createElement("h1");
   let text;
-
-  var name = prompt("Enter your name : ");
+  var name = prompt("Enter your name: ");
   if (name == null || name == "") {
     text = "No name found";
   } else {
-    text = "Hellow " + name;
+    text = "Hello" + name;
   }
   var textNode = document.createTextNode(text);
   h1.appendChild(textNode);
